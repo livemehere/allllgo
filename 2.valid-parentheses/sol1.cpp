d3694356c4bd05351afc1de8c7b222e0bfeebf1b
livemehere <rhdxoals@gmail.com>
2025-04-07 23:56:12 +0900

    valid-parentheses cpp

---
+++ 2.valid-parentheses/sol1.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <stack>
+
+using namespace std;
+
+class Solution {
+    public:
+        bool isValid(string s) {
+            unordered_map<char,char> map = {
+                {'(',')'},
+                {'[',']'},
+                {'{','}'}
+            };
+            stack<char> stack;
+
+            for(char c : s){
+                if(map.find(c) != map.end()){
+                    stack.push(map[c]);
+                } else {
+                    if(stack.empty() || stack.top() != c) return false;
+                    stack.pop();
+                }
+            }
+
+            return stack.empty();
+        }
+};
+
+
+int main(){
+    Solution s;
+    string s1 = "()";
+    string s2 = "()[]{}";
+    string s3 = "(]";
+    string s4 = "([)]";
+    string s5 = "{[]}";
+    string s6 = "){";
+
+    cout << "Result: " << s.isValid(s1) << endl;
+    cout << "Result: " << s.isValid(s2) << endl;
+    cout << "Result: " << s.isValid(s3) << endl;
+    cout << "Result: " << s.isValid(s4) << endl;
+    cout << "Result: " << s.isValid(s5) << endl;
+    cout << "Result: " << s.isValid(s6) << endl;
+     
+
+    return 0;
+}
