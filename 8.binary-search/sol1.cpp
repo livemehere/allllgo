e3011170e8c9c1891ae147195e204b5d39c671cd
livemehere <rhdxoals@gmail.com>
2025-04-14 23:39:14 +0900

    8.binary-search cpp

---
+++ 8.binary-search/sol1.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    public:
+        int search(vector<int>& nums, int target) {
+            
+        }
+    };
+int main(){
+    return 0;
+}
