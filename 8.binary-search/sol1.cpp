ac20a4463cb754a0981c610e8d4758cb4dd59a18
livemehere <rhdxoals@gmail.com>
2025-04-14 23:39:23 +0900

    8.binary-search cpp

--- 8.binary-search/sol1.cpp
+++ 8.binary-search/sol1.cpp
@@ -5,7 +5,19 @@ using namespace std;
 class Solution {
     public:
         int search(vector<int>& nums, int target) {
-            
+            int left=0,right=nums.size()-1;
+
+            while(left<=right){
+                int mid = (left+right)/2;
+                int v = nums[mid];
+                if(v==target) return mid;
+                if(v>target){
+                    right=mid-1;
+                }else{
+                    left=mid+1;
+                }
+            }
+            return -1;
         }
     };
 int main(){
