6b48c02b70e5d57751faf8a8ec807dbae85dae28
livemehere <rhdxoals@gmail.com>
2025-06-03 15:21:05 +0900

    add cycle detection solution for linked lists in JavaScript

---
+++ 12.linked-list.cycle/so3.js
@@ -0,0 +1,19 @@
+ function ListNode(val) {
+     this.val = val;
+     this.next = null;
+ }
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    const visited = new Set();
+    let cur = head;
+    while(cur){
+        if(visited.has(cur)) return true;
+        visited.add(cur);
+        cur = head.next;
+    }
+    return false
+};
