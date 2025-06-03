6e2de3c496ac3d8a878f9b069d42c3c4baaa186e
livemehere <rhdxoals@gmail.com>
2025-06-03 18:46:30 +0900

    refactor: optimize first bad version solution by removing unnecessary array creation

--- 14.fist-bad-version/sol1.js
+++ 14.fist-bad-version/sol1.js
@@ -18,16 +18,16 @@ var solution = function(isBadVersion) {
      * @return {integer} The first bad version
      */
     return function(n) {
-        const arr = Array.from({length:n},(_,i)=> i+1);
-        const len = arr.length;
         let latestBadVersion;
-        for(let i=len-1;i>=0;i--){
-            const version = arr[i];
-            if(isBadVersion(version)){
-                latestBadVersion = version;
+        let cur = n;
+        while(cur !== 0){
+            
+            if(isBadVersion(cur)){
+                latestBadVersion = cur;
             }else{
-                return version + 1;
+                return cur + 1;
             }
+            cur--;
         }
         return latestBadVersion;
     };
@@ -35,4 +35,3 @@ var solution = function(isBadVersion) {
 
 const fn = solution((n)=>{ return n >= 4})
 console.log(fn(5));
-;
