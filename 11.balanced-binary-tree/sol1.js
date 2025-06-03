23d5c3868dc44230c9c154a9b12534aaff33ed69
livemehere <rhdxoals@gmail.com>
2025-06-03 14:00:12 +0900

    refactor balanced binary tree solution for clarity and efficiency

--- 11.balanced-binary-tree/sol1.js
+++ 11.balanced-binary-tree/sol1.js
@@ -1,26 +1,21 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+function TreeNode(val, left, right) {
+     this.val = (val===undefined ? 0 : val)
+     this.left = (left===undefined ? null : left)
+     this.right = (right===undefined ? null : right)
+}
 /**
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isBalanced = function (root) {
-  function height(node) {
-    if (node === null) return 0;
-
-    const leftH = height(node.left);
-    const rightH = height(node.right);
-
-    if (Math.abs(leftH - rightH) > 1) return -1;
-    if (leftH === -1 || rightH === -1) return -1;
-    return Math.max(leftH, rightH) + 1;
-  }
-
-  return height(root) === -1;
+var isBalanced = function(root) {
+    return getHeight(root) !== -1;
 };
+
+function getHeight(node, depth = 0){
+    if(!node || (!node.left && !node.right)) return depth; 
+    const l = node.left ? getHeight(node.left, depth+1) : depth; 
+    const r = node.right ? getHeight(node.right, depth+1) : depth;
+    if(l == -1 || r == -1) return -1;
+    if(Math.abs(l-r) > 1) return -1;
+    return Math.max(l,r);
+}
