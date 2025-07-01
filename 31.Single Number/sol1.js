dc36169421a58c457dedaeede92e159dac9e8bb0
livemehere <rhdxoals@gmail.com>
2025-07-01 16:00:40 +0900

    31.js

--- 31.Single Number/sol1.js
+++ 31.Single Number/sol1.js
@@ -3,5 +3,19 @@
  * @return {number}
  */
 var singleNumber = function(nums) {
-    
+  const m = new Map();
+  for(const n of nums){
+    const v = m.get(n);
+    // first
+    if(v == undefined){
+        m.set(n,1);
+        continue;
+    }
+    // second times
+    // remove n from arr
+    if(v == 1){
+        m.delete(n);
+    }
+  }  
+  return m.keys().next().value;
 };
