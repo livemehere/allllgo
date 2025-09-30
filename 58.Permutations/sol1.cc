4a6e8b515d6b0b1c588daf02ed44d782534067d0
livemehere <rhdxoals@gmail.com>
2025-10-01 00:17:31 +0900

    58.wip

---
+++ 58.Permutations/sol1.cc
@@ -0,0 +1,35 @@
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> res;
+    vector<int> path;
+    vector<vector<int>> permute(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            get(i)
+        }
+        return res;
+    }
+
+    vector<vector<int>> get(vector<int> arr)
+    {
+        vector<int> slice = copy(arr.begin() + 1, );
+
+        return copy;
+    }
+};
+
+int main()
+{
+
+    Solution s;
+
+    return 0;
+}
